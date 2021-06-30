596466b63cd9729d06c41d4646e86806f9891596
Aicha2301 <fatou.aissatou.99@gmail.com>
2021-06-30 23:32:21 +0000

    Creation du projet

---
+++ app/src/main/java/com/example/paybill/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.paybill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
