4e260880f0c4641852ace8c2847cf80f78777d1e
Aicha2301 <fatou.aissatou.99@gmail.com>
2021-07-02 13:26:16 +0000

    Realisation du fragment nav_header_main

--- app/src/main/java/com/example/paybill/MainActivity.java
+++ app/src/main/java/com/example/paybill/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.paybill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtLogin, txtPassword ;
+    private Button btnConnect , btnSignUp ;
+    private String login , password ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConnect = findViewById(R.id.btnConnect);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,UserActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
